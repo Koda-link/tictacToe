01b59dbac4c856153001781ad52332aee08ffaf6
Koda-link <257644640+Koda-link@users.noreply.github.com>
2026-08-27 15:49:48 -0600

    code adapted as factory functions & module pattern

--- jscript.js
+++ jscript.js
@@ -1,105 +1,106 @@
-function setGame(p1, p2, sym){
+function setGame(name1, name2, whoMark){
     let board = [
         [2, 3, 4], 
         [5, 6, 7], 
         [8, 9, 2]
     ];
-    let players = [
-        {
-            name: ``,
-            symbol: 2,
-            wins: 0
-        }, {
-            name: ``,
-            symbol: 2,
-            wins: 0
-        }
-    ]
+    let players = [];
+    function newPlayer(name){
+        let mark = 2;
+        let wins = 0;
+        const won = () => {wins++;};
+        const getWin = () => wins;
+        return {name, mark, getWin, won}
+    };
 
-    players[0].name = p1;
-    players[1].name = p2;
-    sym === true ? (players[0].symbol = 0, players[1].symbol = 1) : (players[0].symbol = 1, players[1].symbol = 0);
-    
-    const getBoard = () => board;
-    const message = `${players[0].name} >> ${players[0].symbol} && ${players[1].name} >> ${players[1].symbol}`;
-    return {message, getBoard};
-}
+    const p1 = newPlayer(`${name1}`);
+    const p2 = newPlayer(`${name2}`);
+    players.push(p1);
+    players.push(p2);
 
-function newGame(){
-    let roundCount = 0;
-    let turnCount = 1;
-    let sym1;
-    let sym2;
-    
+    whoMark === true ? (players[0].mark = 0, players[1].mark = 1) : (players[0].mark = 1, players[1].mark = 0);
+
+    const turns = { t1: 2, t2: 2 };
     const firsTurn = () => {
         return Math.floor(Math.random() * 2) === 0 ? 
-        (sym1 = players[0].symbol, sym2 = players[1].symbol) : (sym2 = players[0].symbol, sym1 = players[1].symbol);
+        (turns.t1 = players[0].mark, turns.t2 = players[1].mark) : (turns.t2 = players[0].mark, turns.t1 = players[1].mark);
     };
+    firsTurn();
+
+    const message = `${players[0].name} >> ${players[0].mark}  && ${players[1].name} >> ${players[1].mark}`;
+
+    const geTurns = () => turns;
+    const getBoard = () => board;
+    const getPlayers = () => players;
+    return {message, getBoard, geTurns, getPlayers};
+};
+
+function newGame(){
+
+    let board = set.getBoard();
+
+    players = set.getPlayers(); 
+
+    // let turns = set.geTurns();
+    const {t1, t2} = turns;
+
+    let roundCount = 0;
+
+    let turnCount = 0;
+    let yourTurn;
+    const geTount = () => turnCount;
     
-    function tach(x, y, symTurn){
-        turnCount % 2 === 0 ? symTurn = sym2 : symTurn = sym1;
-        board[x].splice(y, 1, symTurn);
-        turnCount++;
-    }
+    // function resetRound(){
+    // turnCount = 1;
+    // board = [
+    //     [2, 3, 4], 
+    //     [5, 6, 7], 
+    //     [8, 9, 2]
+    // ];
+    // roundCount++;
+    // }
 
-    function resetRound(){
-    turnCount = 1;
-    board = [
-        [2, 3, 4], 
-        [5, 6, 7], 
-        [8, 9, 2]
-    ];
-    roundCount++;
+    function marking(x, y){
+        turnCount += 1;
+        turnCount % 2 === 0 ? yourTurn = t2 : yourTurn = t1;
+        board[x].splice(y, 1, yourTurn);
+        if(turnCount >= 5){return check()};
+        return {board, turnCount};
     }
+    
 
-    function check(board){
-        for(let i = 0 ; i > 2; i++){
-            if(board => board.every( i => i === board[i][0] )){
-                board[i][0] === players[0].symbol ? players[0].wins++ : players[1].wins++;
-                resetRound();
+    function check(){
+        console.log(`checking`);
+        for(let i = 0 ; i < 2; i++){
+            if(board[i].every((e) => e === board[i][e])){
+                return board[i][0] === players[0].mark ? players[0].won(): players[1].won();
+                // resetRound();
             }
             else if(board[0][i] === board[1][i] && board[1][i] === board[2][i]){
-               board[0][i] === players[0].symbol ? players[0].wins++ : players[1].wins++;
-               resetRound()
+               return board[0][i] === players[0].mark ? players[0].won() : players[1].won();
+            //    resetRound()
             }
-
         }if((board[1][1] == board[0][0] && board[1][1] == board[2][2]) || 
             (board[1][1] == board[0][2] && board[1][1] == board[2][0])){
-                board[1][1] === players[1].symbol ? players[1].wins++ : players[0].wins++;
-                resetRound();
-            } 
-    }if(turncount >= 5){check()}
-    else if(turncount >= 9){resetRound()}
-    
-    let winArchive = [];
-    winArchive.push(players[0].symbol)
-    let racha = 3;
-    if(winArchive[winArchive.length - 1] === winArchive[winArchive.length - 2] && 
-        winArchive[winArchive.length - 1] === winArchive[winArchive.length - 3]){
-        winArchive[0] === players[0].symbol ? `Congrats ${players[0].name}! ${racha} a row`: `Congrats ${players[1].name}! ${racha} a row`;
-        racha++;
-    }else if(winArchive[winArchive.length - 1] !== winArchive[winArchive.length - 2]){racha = 3;}
-
-}
-function resetGame(){
-    board = [
-        [2, 3, 4], 
-        [5, 6, 7], 
-        [8, 9, 2]
-    ];
-    players = [
-        {
-            name: ``,
-            symbol: 2,
-            wins: 0
-        }, {
-            name: ``,
-            symbol: 2,
-            wins: 0
+                return board[1][1] === players[1].mark ? players[1].won() : players[0].won();
+            // resetRound();
         }
-    ]
-    players[0].name = p1;
-    players[1].name = p2;
-    sym === true ? (players[0].symbol = 0, players[1].symbol = 1) : (players[0].symbol = 1, players[1].symbol = 0);
+        // else if(turnCount == 9){return resetRound()}
+    }return {marking, board, players, turns};
+
+    
+    // let winArchive = [];
+    // winArchive.push(players[0].symbol)
+    // let racha = 3;
+    // if(winArchive[winArchive.length - 1] === winArchive[winArchive.length - 2] && 
+    //     winArchive[winArchive.length - 1] === winArchive[winArchive.length - 3]){
+    //     winArchive[0] === players[0].symbol ? `Congrats ${players[0].name}! ${racha} a row`: `Congrats ${players[1].name}! ${racha} a row`;
+    //     racha++;
+    // }else if(winArchive[winArchive.length - 1] !== winArchive[winArchive.length - 2]){racha = 3;}
 
 }
+const set = setGame(`Ginni`, `Bo`, false);
+const now = newGame(); 
+// function resetGame(){
+
+// }
